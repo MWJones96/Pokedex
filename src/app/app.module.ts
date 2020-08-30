8049f0546c64e71b102a69fc8289cfe13e92ffcf
Matthew Jones <jonesmat@usc.edu>
2020-08-30 21:30:41 +0100

    More

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,27 @@ import { AppComponent } from './app.component';
 import { PokedexComponent } from './pokedex/pokedex.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
 
+let typeMap = new Map([
+  ["normal", ""],
+  ["fight", ""],
+  ["flying", ""],
+  ["poison", "#a040a0"],
+  ["ground", ""],
+  ["rock", ""],
+  ["bug", ""],
+  ["ghost", ""],
+  ["steel", ""],
+  ["fire", ""],
+  ["water", ""],
+  ["grass", "#78c850"],
+  ["electric", ""],
+  ["psychic", ""],
+  ["ice", ""],
+  ["dragon", ""],
+  ["dark", ""],
+  ["fairy", ""]
+]);
+
 @NgModule({
   declarations: [
     AppComponent,
